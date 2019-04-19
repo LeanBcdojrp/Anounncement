9f9d290d8ea44104eae6ee7c1073f27091920e2d
Aidan Atkins <49736260+LeanBcdojrp@users.noreply.github.com>
2019-04-19 09:35:10 -0400

    Add files via upload

---
+++ announce/server.lua
@@ -0,0 +1,11 @@
+RegisterServerEvent("announce")
+AddEventHandler("announce", function(param)
+    print("[^1Announcement^7]" .. param)
+    TriggerClientEvent("chatMessage", -1, "^7[^4---------^1Announcement^5---------^7]", {0,0,0}, param)
+end)
+
+
+-- ^1
+-- ^2
+-- ^3
+--etc
